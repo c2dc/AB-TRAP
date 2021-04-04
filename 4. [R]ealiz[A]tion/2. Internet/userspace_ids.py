c63fbfdb82c90146d96a42799f9d146352598e0e
Gustavo Bertoli <gubertoli@gmail.com>
2021-04-04 19:34:01 -0300

    ids working for RF

--- 4. [R]ealiz[A]tion/2. Internet/userspace_ids.py
+++ 4. [R]ealiz[A]tion/2. Internet/userspace_ids.py
@@ -7,9 +7,11 @@ queue = 1
 
 conn = fnfqueue.Connection()
 preprocess = load(open("Models/preprocessor.pkl", "rb"))
-model = load(open("Models/xgb.pkl", "rb"))
+model = load(open("Models/rf.pkl", "rb"))
 
-features = ['ip_id', 'ip_flag_df', 'ip_ttl', 'ip_len', 'ip_dsfield', 'tcp_dport', 'tcp_seq', 'tcp_len', 'tcp_hdr_len', 'tcp_flag_fin', 'tcp_flag_syn', 'tcp_flag_rst', 'tcp_flag_push', 'tcp_flag_ack', 'tcp_flag_urg', 'tcp_flag_cwr', 'tcp_window_size', 'tcp_urgent_pointer', 'tcp_options_mss_val']
+features = ['ip.id', 'ip.flag.df', 'ip.ttl', 'ip.len', 'ip.dsfield', 'tcp.srcport', 'tcp.seq', 'tcp.len', 'tcp.hdr_len',
+            'tcp.flags.fin', 'tcp.flags.syn', 'tcp.flags.rst', 'tcp.flags.push', 'tcp.flags.ack', 'tcp.flags.urg',
+            'tcp.flags.cwr', 'tcp.window_size', 'tcp.urgent_pointer', 'tcp.options.mss_val']
 
 try:
     q = conn.bind(queue)
@@ -31,7 +33,7 @@ while True:
             pkt.append(l3.len)   #ip_len
             pkt.append(l3.tos)     #ip_dsfield
 
-            pkt.append(l4.dport) #tcp_dport
+            pkt.append(l4.dport) #tcp_dport !!!!!!
             pkt.append(l4.seq)   #tcp_seq
             pkt.append(len(l4.payload))  #tcp_len
             pkt.append(len(l4))  #tcp_hdr_len
@@ -46,16 +48,18 @@ while True:
             pkt.append(l4.flags.C & 1)   #tcp_flag_cwr
             pkt.append(l4.window)        #tcp_window_size
             pkt.append(l4.urgptr)        #tcp_urgent_pointer
-            pkt.append(next((x[1] for x in l4.options if x[0] == "MSS"), 0)) #tcp_options_mss_val
-            
-            df = DataFrame(pkt, columns=features)
+            # to retrieve mss_val is required to look for this field in the TCP options
+            # normally this field is in the byte format, so the convertion to int from bytes is required
+            # if the field MSS is not set, the value is used as zero
+            pkt.append(next((int.from_bytes(x[1], byteorder=sys.byteorder) for x in l4.options if x[0] == "MSS"), 0)) #tcp_options_mss_val
+
+            df = DataFrame([pkt], columns=features)
             X = preprocess.transform(df)
             predict = model.predict(X)
-            
-            if l3.src == "192.168.0.1" and l3.dst == "192.168.0.2":
+
+            if not predict:
                 packet.accept()
             else:
-                print(predict)
                 packet.drop()
 
     except fnfqueue.BufferOverflowException:
