4a2df1ec69b40b6e691f43f18efbaaeb48b94b73
Gustavo Bertoli <gubertoli@gmail.com>
2021-04-05 09:56:49 -0300

    udp server changes

--- 5. [P]erformance Evaluation/2. Internet/udp_server.py
+++ 5. [P]erformance Evaluation/2. Internet/udp_server.py
@@ -15,35 +15,34 @@ print (">> Flushing IPTables rules")
 os.system('iptables -F')
 
 while True:
-    message, client_address = server_socket.recvfrom(2048)
-    print(">> Received message from: ", client_address[0])
 
     msg, client_addr = server_socket.recvfrom(2048)
     model_name = msg.decode('utf-8')
+    #print(">> Received message from: ", client_address[0])
 
     accept_model = ['knn', 'rf', 'dt', 'lr', 'xgb', 'mlp', 'nb', 'svm']
-
+    
     if model_name in accept_model:
-        print(">> Recognized model ({0})".format(model_name))
+        print(">> *** Recognized model ({0}) ***".format(model_name))
         run_iperf = subprocess.Popen(['iperf3', '-s', '-D'])
         print(">> Running IPerf3 as a Daemons (PID={0})".format(str(run_iperf.pid)))
 
-        print(">> Create IPTables rule for NFQueue (Queue=1")
+        print(">> Create IPTables rule for NFQueue (Queue=1)")
         os.system("iptables -A INPUT -p tcp -j NFQUEUE --queue-num 1")
 
 
-        run_ids = subprocess.Popen(['../../venv/bin/python',
-                                    '../../4.\ \[R\]ealiz\[A\]tion/2.\ Internet/userspace_ids.py',
-                                    model_name], stdout=subprocess.PIPE)
+        run_ids = subprocess.Popen(['../../venv/bin/python', 'userspace_ids.py', str(model_name)], stdout=subprocess.PIPE)
         print(">> Running IDS with {0} model (PID={1})".format(model_name, str(run_ids.pid)))
-        print(">> Running sar to capture sysstat data")
-        os.system("sar -r ALL -n ALL -u ALL -o {0} {1} {2}".format(model_name, SAMPLE_RATE, RECORDING_TIME))
-
+        
+        run_sar = subprocess.Popen(['sar', '-r', 'ALL', '-n', 'ALL', '-u', 'ALL', '-o', str(model_name), str(SAMPLE_RATE), str(RECORDING_TIME)], stdout=subprocess.PIPE)
+        print(">> Running sar to capture sysstat data (PID={0})".format(str(run_sar.pid)))
+        
     if model_name == "STOP":
         try:
-            print(">> Stopped PID:", str(run_ids.pid), str(run_iperf.pid))
+            print(">> Stopped PID:", str(run_ids.pid), str(run_iperf.pid), str(run_sar.pid))
             run_iperf.terminate()
             run_ids.terminate()
+            run_sar.terminate()
         except:
             print(">> Error to terminate process")
 
