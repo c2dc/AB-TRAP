2487d0ce70ced4bcd60e2f32b1d7546e0780aa7c
Gustavo Bertoli <gubertoli@gmail.com>
2021-04-05 02:22:27 -0300

    minor changes to udp server

--- 5. [P]erformance Evaluation/2. Internet/udp_server.py
+++ 5. [P]erformance Evaluation/2. Internet/udp_server.py
@@ -11,7 +11,7 @@ server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(('', server_port))
 
 print (">> Server running...")
-
+print (">> Flushing IPTables rules")
 os.system('iptables -F')
 
 while True:
@@ -24,11 +24,19 @@ while True:
     accept_model = ['knn', 'rf', 'dt', 'lr', 'xgb', 'mlp', 'nb', 'svm']
 
     if model_name in accept_model:
+        print(">> Recognized model ({0})".format(model_name))
         run_iperf = subprocess.Popen(['iperf3', '-s', '-D'])
+        print(">> Running IPerf3 as a Daemons (PID={0})".format(str(run_iperf.pid)))
+
+        print(">> Create IPTables rule for NFQueue (Queue=1")
+        os.system("iptables -A INPUT -p tcp -j NFQUEUE --queue-num 1")
+
+
         run_ids = subprocess.Popen(['../../venv/bin/python',
                                     '../../4.\ \[R\]ealiz\[A\]tion/2.\ Internet/userspace_ids.py',
                                     model_name], stdout=subprocess.PIPE)
-        os.system("iptables -A INPUT -p tcp -j NFQUEUE --queue-num 1")
+        print(">> Running IDS with {0} model (PID={1})".format(model_name, str(run_ids.pid)))
+        print(">> Running sar to capture sysstat data")
         os.system("sar -r ALL -n ALL -u ALL -o {0} {1} {2}".format(model_name, SAMPLE_RATE, RECORDING_TIME))
 
     if model_name == "STOP":
