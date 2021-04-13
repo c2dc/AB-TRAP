4b04412753eb9f04e64db0fb1f9575e4f17c7a61
Gustavo Bertoli <gubertoli@gmail.com>
2021-04-13 17:06:51 -0300

    implemented script to average predict time for each model

--- 5. [P]erformance Evaluation/2. Internet/inferencing_time.py
+++ 5. [P]erformance Evaluation/2. Internet/inferencing_time.py
@@ -1,15 +1,21 @@
 import timeit
+import random
 from pickle import load
 from pandas import DataFrame
 
 models = ['knn', 'rf', 'dt', 'lr', 'xgb', 'mlp', 'nb', 'svm']
 
-features = []
+features = ['ip.id', 'ip.flags.df', 'ip.ttl', 'ip.len', 'ip.dsfield', 'tcp.srcport', 'tcp.seq', 'tcp.len', 'tcp.hdr_len',
+           'tcp.flags.fin', 'tcp.flags.syn', 'tcp.flags.reset', 'tcp.flags.push', 'tcp.flags.ack', 'tcp.flags.urg',
+           'tcp.flags.cwr', 'tcp.window_size', 'tcp.urgent_pointer', 'tcp.options.mss_val']
 
-preprocess = load(open("../../4/2/Models/preprocessor.pkl", "rb"))
+preprocess = load(open("../../4. [R]ealiz[A]tion/2. Internet/Models/preprocessor.pkl", "rb"))
+
+# generating random attributes 
+pkt = [random.random() for i in range(0,len(features))]
 
 for model in models:
-    pickle_model = load(open("../../4/2/Models/"+model+".pkl","rb"))
+    pickle_model = load(open("../../4. [R]ealiz[A]tion/2. Internet/Models/"+model+".pkl","rb"))
 
     df = DataFrame([pkt], columns=features)
 
@@ -20,4 +26,6 @@ for model in models:
     X = preprocess.transform(df)
     X = DataFrame(X, columns=features)
 
-    print("The inference time taken by {0} is {1}".format(model, timeit.timeit(stmt=pickle_model.predict(X))))
+    print("The average inference time taken by {} is {:.2f} ms".format(model, timeit.timeit('pickle_model.predict(X)',
+                                                                               setup="from __main__ import pickle_model, X",
+                                                                               number=1000)))
